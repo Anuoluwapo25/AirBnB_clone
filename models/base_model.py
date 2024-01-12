eb76c5d357fc42cfc9b6d93e1821de1b5bb775b0
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-01-12 02:31:48 -0800

    kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,24 @@
 from uuid import uuid4
 from datetime import datetime
 class BaseModel:
-    def __init__(self) -> None:
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid4())
+            if not any(key in kwargs for key in ["created_at", "updated_at"]):
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         
     def __str__(self) -> str:
         return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
