8bcf276195f2418befb654ed1d987d2abecd7353
Temesgen dora <tdemeke36@gmail.com>
2024-01-13 00:15:11 -0800

    fixed: console

--- console.py
+++ console.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import cmd
-class HBNBCommand(cmd.Cmd):
+from cmd import Cmd
+
+class HBNBCommand(Cmd):
     prompt = "(hbnb) "
     def do_quit(self, line):
         return True
     def help_quit(self):
         print ('Quit command to exit the program\n')
+    def emptyline(self):
+        pass
         
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
