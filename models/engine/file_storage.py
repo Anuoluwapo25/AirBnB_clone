8fcca8cf6cd3a7fe1d8ed8c9fc93fc2c16aabcae
Temesgen dora <tdemeke36@gmail.com>
2024-01-12 23:53:13 -0800

    done: task 6

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module for file storage"""
-from model.base_model import BaseModel
+from models.base_model import BaseModel
 import json
 import os
 
---
+++ test/test_models/test_base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+
+
+class test_base_model(unittest.TestCase):
+    def test_id(self):
+        self.assertEqual(type(BaseModel.id), str)
+    
