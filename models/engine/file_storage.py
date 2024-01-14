aa6478f6ff8dffb2413e1218e9c1e1251b9c3c57
Temesgen dora <tdemeke36@gmail.com>
2024-01-14 00:07:58 -0800

    json

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,12 +8,12 @@ class FileStorage():
     __file_path = "file.json"
     __objects = {}
 
-    @property
+
     def all(self):
         """returns the d__objects"""
         return self.__objects
     
-    @all.setter
+
     def new(self, obj):
         """sets in __objects the obj with key"""
         keyo = f"{obj.__class__.__name__}.{obj.id}"
@@ -42,5 +42,4 @@ class FileStorage():
 
                 except json.JSONDecodeError:
                     pass
- 
-
+   
