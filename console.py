c5f67b9225c295710fc639b7531c65a9324636ab
Temesgen dora <tdemeke36@gmail.com>
2024-01-14 00:27:10 -0800

    console

--- console.py
+++ console.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
-from cmd import Cmd
+import cmd
 from models.base_model import BaseModel
 import sys
-class HBNBCommand(Cmd):
+
+
+class HBNBCommand(cmd.Cmd):
+
     prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        return True
+
+    def help_EOF(self, command):
+        print("EOF Command to exit the program\n")
+
     def do_quit(self, line):
         return True
+
     def help_quit(self):
-        print ('Quit command to exit the program\n')
+        print('Quit command to exit the program\n')
+
     def emptyline(self):
         pass
+
     def do_create(self, class_name):
         if class_name:
             if class_name in globals():
                 basemodel_instance = BaseModel()
-                
+
             else:
                 print("** class doesn't exist **")
 
         elif not class_name:
-            print ("** class name missing **")
+            print("** class name missing **")
+
     def do_show(self, class_name):
         if class_name:
             if class_name in globals():
@@ -30,7 +44,8 @@ class HBNBCommand(Cmd):
                 print("** class doesn't exist **")
 
         elif not class_name:
-            print ("** class name missing **")
+            print("** class name missing **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
