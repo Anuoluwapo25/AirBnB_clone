a86f97874ca9e22c780c5dd9b2e0aab5f3974953
Temesgen dora <tdemeke36@gmail.com>
2024-01-14 00:38:33 -0800

    console

--- console.py
+++ console.py
@@ -1,30 +1,68 @@
 #!/usr/bin/python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
 import cmd
 from models.base_model import BaseModel
 import sys
 
 
 class HBNBCommand(cmd.Cmd):
+    """_summary_
 
+    Args:
+        cmd (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     prompt = '(hbnb) '
 
     def do_EOF(self, arg):
+        """_summary_
+
+        Args:
+            arg (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         print()
         return True
 
     def help_EOF(self):
+        """_summary_
+        """
         print("Quit command to exit the program")
 
     def do_quit(self, line):
+        """_summary_
+
+        Args:
+            line (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         return True
 
     def help_quit(self):
+        """_summary_
+        """
         print('Quit command to exit the program')
 
     def emptyline(self):
         pass
 
     def do_create(self, class_name):
+        """_summary_
+
+        Args:
+            class_name (_type_): _description_
+        """
         if class_name:
             if class_name in globals():
                 basemodel_instance = BaseModel()
@@ -36,6 +74,11 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def do_show(self, class_name):
+        """_summary_
+
+        Args:
+            class_name (_type_): _description_
+        """
         if class_name:
             if class_name in globals():
                 basemodel_instance = BaseModel()
